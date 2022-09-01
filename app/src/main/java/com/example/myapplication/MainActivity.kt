52ecbb9d649a7a98d828bb60d5831c05ee9b1e2a
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2022-09-01 12:22:26 +0900

    [feat] 메인화면 추가

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,47 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button1.setOnClickListener(View.OnClickListener {
+            Toast.makeText(
+                this,
+                "로그인화면",
+                Toast.LENGTH_LONG
+            ).show()
+        })
+        binding.button2.setOnClickListener(View.OnClickListener {
+            Toast.makeText(
+                this,
+                "검색",
+                Toast.LENGTH_LONG
+            ).show()
+        })
+        binding.button3.setOnClickListener(View.OnClickListener {
+            Toast.makeText(
+                this,
+                "오늘의 식물 추천!",
+                Toast.LENGTH_LONG
+            ).show()
+        })
+        binding.button4.setOnClickListener(View.OnClickListener {
+            Toast.makeText(
+                this,
+                "오늘의 날씨!",
+                Toast.LENGTH_LONG
+            ).show()
+        })
     }
 
 }
